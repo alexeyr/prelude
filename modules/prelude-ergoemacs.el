c48261c4fc50ef6c04745ce4d9b8d08613fc6400
Alexey Romanov <alexey.v.romanov@huawei.com>
2014-06-30 11:55:45 +0400

    Add Ergoemacs module

---
+++ modules/prelude-ergoemacs.el
@@ -0,0 +1,41 @@
+;;; prelude-ergoemacs.el --- Emacs Prelude: Standard cross-platform shortcuts.
+;;
+;; Copyright © 2014 Alexey Romanov
+;;
+;; Author: Alexey Romanov <alexey.v.romanov@gmail.com>
+;; URL: https://github.com/bbatsov/prelude
+;; Version: 1.0.0
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Use Ergoemacs to provide standard cross-platform shortcuts
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+(prelude-require-packages '(ergoemacs-mode))
+
+(setq ergoemacs-theme nil) ;; Uses Standard Ergoemacs keyboard theme
+(setq ergoemacs-keyboard-layout "us") ;; Assumes QWERTY keyboard layout
+(ergoemacs-mode 1)
+
+(provide 'prelude-ergoemacs)
